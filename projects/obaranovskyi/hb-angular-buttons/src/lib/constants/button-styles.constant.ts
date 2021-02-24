58a25914e398091e818977b3817558dff7a61d57
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2021-02-24 10:06:08 +0200

    Auto git push ...

--- projects/obaranovskyi/hb-angular-buttons/src/lib/constants/button-styles.constant.ts
+++ projects/obaranovskyi/hb-angular-buttons/src/lib/constants/button-styles.constant.ts
@@ -4,11 +4,6 @@ import { KeyValue } from '../types';
 import { asCss } from '../core';
 
 
-/*
- *export const lightBtnStyle: KeyValue = btnAsCss('white', 'black');
- *export const darkBtnStyle: KeyValue = btnAsCss('black', 'white');
- */
-
 export const commonBtnStyle: KeyValue = asCss(`
   padding: 10px 15px;
   font-size: 14px;
